2b19f5a702395c30f19525f48e548aca30d1ee78
Adithyadinesh21 <work.adithyadinesh@gmail.com>
2023-12-16 22:39:59 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,189 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Software Engineer Intern",
+        company_name: "Stripe",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "Nov 2023 - Present",
+        points: [
+            "Collaborated with a dynamic team of software engineers to design and implement key functionalities, gaining hands-on experience with modern technologies and tools in the fintech industry.",
+            "Demonstrated a strong problem-solving aptitude by effectively identifying and addressing challenges during the development lifecycle, contributing to the overall improvement of software design and functionality.",
+        ],
+    },
+    {
+        title: "Software Development Engineer Intern",
+        company_name: "Swiggy",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "July 2023 - Oct 2023",
+        points: [
+            "Implemented performance improvements that resulted in a 20% reduction in app loading times, enhancing user experience and retention rates.",
+            "Conducted thorough code reviews and identified critical bugs, leading to a 30% decrease in app crashes and improved overall app stability.",
+        ],
+    },
+    {
+        title: "Software Development Engineer Intern",
+        company_name: "BEO Software Private LTD",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "May 2023 - July 2023",
+        points: [
+            "worked closely with the development team, leveraging my skills in Dart programming language and Flutter framework to create intuitive and user-friendly mobile applications.",
+            "successfully integrated several APIs into the Flutter projects I worked on. These APIs ranged from social media platforms, payment gateways, mapping services, to weather data providers, among others.",
+        ],
+    },
+    {
+        title: "Expert",
+        company_name: "Chegg",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Sept 2022 - Present",
+        points: [
+            "Demonstrated expertise in various computer science topics, including data structures, algorithms, programming languages, and software development.",
+            "Assisted students in understanding complex concepts and solving programming problems through clear explanations and step-by-step guidance.",
+            "Provided assistance to over 500+ students in resolving their doubts and clarifying concepts in computer science.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/AdithyaDinesh21',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/adithya-dinesh-77990026b/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'NextUniswap',
+        description: 'Decentralized Cryptocurrency Exchange React App with Metamask Pairing & Swap ERC20 Tokens!',
+        link: 'https://github.com/RealAdii/NextUniswap',
+    },
+    
+    
+];
